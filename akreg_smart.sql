7c6e05a5e53c5a03e3e7acb54b969913d7e2abab
marcossmart <38054827+marcossmart@users.noreply.github.com>
2018-06-28 17:57:08 -0300

    Commit 2 archivo SQL

---
+++ akreg_smart.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: akreg_smart
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.31-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) NOT NULL,
+  `apellido` varchar(25) NOT NULL,
+  `usuario` varchar(25) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `avatar` varchar(200) NOT NULL DEFAULT 'sinfoto.jpg',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Julian','Drets','akreg96','juliandrets@gmail.com','asdasd','sinfoto.jpg'),(3,'Roberto','Carlos','robercar','asd@asd.asd','roberto','sinfoto.jpg'),(4,'Marcos','Smart','msmart','smartmarcos@hotmail.com','$2y$10$0z5lGKpyiNSIh','sinfoto.jpg'),(5,'Marcos','Smart','marcossmart','smartmarcos@hotmail.com','$2y$10$3HlUWcOr/3adj','sinfoto.jpg');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-28 17:53:54
--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
-	
-	$dsn = 'mysql:host=127.0.0.1;dbname=smart2;port=3306';
+
+	$dsn = 'mysql:host=127.0.0.1;dbname=akreg_smart;port=3306';
 	$db_user = 'root';
 	$db_pass = '';
 	$opt = [ PDO::ATTR_ERRMODE
 	 => PDO::ERRMODE_EXCEPTION ];
 	$link = new PDO($dsn, $db_user, $db_pass, $opt);
 
-	
+
 ?>
